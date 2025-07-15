53da9606facb60b09ea12694b35733130d16f982
Yazdan Ranjbar <yazdanra@icloud.com>
2025-07-15 18:13:25 -0400

    feat: Update macOS options to include hidden and callback functionality

--- tests/test_cli_export_requires_macos.py
+++ tests/test_cli_export_requires_macos.py
@@ -4,37 +4,26 @@ from click.testing import CliRunner
 
 from osxphotos.cli import export
 
-TEST_DB = "tests/Test-15.4.1.photoslibrary"
-
-
-def test_add_exported_to_album_option_hidden(monkeypatch):
-    """Option should not appear in --help on non-macOS"""
-    runner = CliRunner()
-
-    monkeypatch.setattr("osxphotos.platform.is_macos", False)
-    result = runner.invoke(export, ["--help"])
-    assert result.exit_code == 0
-    assert "--add-exported-to-album" not in result.output
-
-    monkeypatch.setattr("osxphotos.platform.is_macos", True)
-    result = runner.invoke(export, ["--help"])
-    assert result.exit_code == 0
-    assert "--add-exported-to-album" in result.output
+TEST_LIBRARY = "./tests/Test-iPhoto-Projects-10.15.7.photoslibrary"
 
 
 def test_add_exported_to_album(monkeypatch):
     """Using option on non-macOS should error"""
+    monkeypatch.setattr(export, "is_macos", False)
+    
     runner = CliRunner()
     cwd = os.getcwd()
-    
-    monkeypatch.setattr("osxphotos.platform.is_macos", False)
+
+    result = runner.invoke(export, ["--help"])
+    assert result.exit_code == 0
+    assert "--add-exported-to-album" not in result.output
     with runner.isolated_filesystem():
         result = runner.invoke(
             export,
             [
                 ".",
                 "--library",
-                os.path.join(cwd, TEST_DB),
+                os.path.join(cwd, TEST_LIBRARY),
                 "--add-exported-to-album",
                 "SomeAlbum",
             ],
@@ -42,14 +31,17 @@ def test_add_exported_to_album(monkeypatch):
         assert result.exit_code == 2
         assert "only works on macOS" in result.output
 
-    monkeypatch.setattr("osxphotos.platform.is_macos", True)
+    monkeypatch.setattr(export, "is_macos", True)
+    result = runner.invoke(export, ["--help"])
+    assert result.exit_code == 0
+    assert "--add-exported-to-album" in result.output
     with runner.isolated_filesystem():
         result = runner.invoke(
             export,
             [
                 ".",
                 "--library",
-                os.path.join(cwd, TEST_DB),
+                os.path.join(cwd, TEST_LIBRARY),
                 "--add-exported-to-album",
                 "SomeAlbum",
             ],
