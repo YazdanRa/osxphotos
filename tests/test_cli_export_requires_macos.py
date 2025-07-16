e411dc9bdb54548735180253512497d80c8bf348
Yazdan Ranjbar <yazdanra@icloud.com>
2025-07-16 00:36:45 -0400

    feat: updated test for export cli required macos

--- tests/test_cli_export_requires_macos.py
+++ tests/test_cli_export_requires_macos.py
@@ -1,22 +1,22 @@
 import os
+from importlib import import_module
 
 from click.testing import CliRunner
 
-from osxphotos.cli import export
+export_module = import_module("osxphotos.cli.export")
 
-TEST_LIBRARY = "./tests/Test-iPhoto-Projects-10.15.7.photoslibrary"
+TEST_LIBRARY = "./tests/Test-10.14.5.photoslibrary"
 
 
 def test_add_exported_to_album(monkeypatch):
     """Using option on non-macOS should error"""
-    monkeypatch.setattr(export, "is_macos", False)
-    
+    monkeypatch.setattr("osxphotos.platform.is_macos", False)
+    monkeypatch.setattr("osxphotos.cli.common.is_macos", False)
+
+    export = export_module.export
     runner = CliRunner()
     cwd = os.getcwd()
 
-    result = runner.invoke(export, ["--help"])
-    assert result.exit_code == 0
-    assert "--add-exported-to-album" not in result.output
     with runner.isolated_filesystem():
         result = runner.invoke(
             export,
@@ -30,20 +30,3 @@ def test_add_exported_to_album(monkeypatch):
         )
         assert result.exit_code == 2
         assert "only works on macOS" in result.output
-
-    monkeypatch.setattr(export, "is_macos", True)
-    result = runner.invoke(export, ["--help"])
-    assert result.exit_code == 0
-    assert "--add-exported-to-album" in result.output
-    with runner.isolated_filesystem():
-        result = runner.invoke(
-            export,
-            [
-                ".",
-                "--library",
-                os.path.join(cwd, TEST_LIBRARY),
-                "--add-exported-to-album",
-                "SomeAlbum",
-            ],
-        )
-        assert result.exit_code == 0
